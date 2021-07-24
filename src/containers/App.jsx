01f943e2372aa23a9890cd139e43a8f206b6d5ec
Joan Rodriguez <joan.rodriguez@alteru.co>
2021-07-23 19:41:38 -0500

    casi terminado

---
+++ src/containers/App.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carrousel from '../components/Carrousel';
+import CarrouselItem from '../components/CarrouselItem';
+import Footer from '../components/Footer';
+import '../assets/styles/App.scss';
+
+const App = () => {
+    const [ videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/initalState')
+        .then(response => response.json())
+        .then(data => setVideos(data));
+    }, []);
+
+    console.log(videos);
+
+    return (
+        <div className='App'>
+            <Header />
+            <Search />
+
+            <Categories title="Mi Lista">
+                <Carrousel>
+                    <CarrouselItem />
+                    <CarrouselItem />
+                    <CarrouselItem />
+                    <CarrouselItem />
+                </Carrousel>
+            </Categories>
+
+            <Categories title="Tendencias">
+                <Carrousel>
+                    <CarrouselItem />
+                    <CarrouselItem />
+                    <CarrouselItem />
+                </Carrousel>
+            </Categories>
+
+
+            <Categories title="Originales de Platzi Video">
+                <Carrousel>
+                    <CarrouselItem />
+                    <CarrouselItem />
+                </Carrousel>
+            </Categories>
+
+
+            <Footer />
+        </div>
+    );
+}
+export default App;
